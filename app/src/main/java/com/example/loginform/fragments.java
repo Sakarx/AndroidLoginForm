1b855a3171b89e9f1e0f580b62173b7dea8ff39e
sakar-resi <sakar@resimator.fi>
2023-04-02 21:36:32 +0545

    Refactored ids

--- app/src/main/java/com/example/loginform/fragments.java
+++ app/src/main/java/com/example/loginform/fragments.java
@@ -3,12 +3,34 @@ package com.example.loginform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class fragments extends AppCompatActivity {
 
+    Button firstFragmentBtn, secondFragmentBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragments);
+
+        firstFragmentBtn = findViewById(R.id.btnFragment1);
+        secondFragmentBtn = findViewById(R.id.btnFragment2);
+
+        firstFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new fragment1());
+            }
+        });
+
+        secondFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new fragment2());
+            }
+        });
+
+
     }
 }
