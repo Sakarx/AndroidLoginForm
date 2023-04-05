90d0ebf88081a7d59e7fec2ce1b514f5274d8282
sakar-resi <sakar@resimator.fi>
2023-04-05 07:06:38 +0545

    Added menu and submenu

--- app/src/main/java/com/example/loginform/Dashboard.java
+++ app/src/main/java/com/example/loginform/Dashboard.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -15,5 +17,13 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainMenu, menu);
+        return true;
     }
 }
