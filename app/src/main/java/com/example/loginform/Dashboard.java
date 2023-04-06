0ff147be63d210cd4eed96cdde4ff47c49e10db7
sakar-resi <sakar@resimator.fi>
2023-04-06 06:44:53 +0545

    Added onOptionsItemSelected for options menu

--- app/src/main/java/com/example/loginform/Dashboard.java
+++ app/src/main/java/com/example/loginform/Dashboard.java
@@ -1,12 +1,15 @@
 package com.example.loginform;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -23,5 +26,20 @@ public class Dashboard extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.menu1){
+            Toast.makeText(this, "Menu1 has been clicked", Toast.LENGTH_SHORT).show();
+        }
+        if(itemId == R.id.menu2){
+            Toast.makeText(this, "Menu2 has been clicked", Toast.LENGTH_SHORT).show();
+        }
+        if(itemId == R.id.menu3){
+            Toast.makeText(this, "Menu1 has been clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
