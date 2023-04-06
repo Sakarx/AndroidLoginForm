a3608fc16caf166ab1f13448913934c469aac720
sakar-resi <sakar@resimator.fi>
2023-04-06 07:09:03 +0545

    Added popup menu

--- app/src/main/java/com/example/loginform/Dashboard.java
+++ app/src/main/java/com/example/loginform/Dashboard.java
@@ -8,12 +8,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    Button popUp = findViewById(R.id.btnPopUp);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +46,38 @@ public class Dashboard extends AppCompatActivity {
             Toast.makeText(this, "Menu1 has been clicked", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showPopup(View view){
+        PopupMenu popupMenu = new PopupMenu(this,view);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        int itemId = menuItem.getItemId();
+        if(itemId == R.id.share){
+            Toast.makeText(this, "share has been clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(itemId == R.id.save){
+            Toast.makeText(this, "save has been clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(itemId == R.id.New){
+            Toast.makeText(this, "New has been clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(itemId == R.id.newFile){
+            Toast.makeText(this, "newFile has been clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if(itemId == R.id.newFolder){
+            Toast.makeText(this, "newFolder has been clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
     }
 }
